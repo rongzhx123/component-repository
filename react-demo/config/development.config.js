413a9314c6f9576fea89b69170abf41ee4079702
rongzhenxin <zx.rong@zuolin.com>
2020-06-28 23:56:26 +0800

    add

---
+++ react-demo/config/development.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '../src/index.js'),
+  output: {
+    filename: 'static/[name].bundle.js',
+    path: path.join(__dirname, '../build'),
+    publicPath: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/react', '@babel/preset-env'],
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../build'),
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'welcome',
+        template: './public/index.html'
+    }),
+  ]
+};
