a2b17897bf6243726822197da02159bcf81a86da
rongzhenxin <zx.rong@zuolin.com>
2020-07-19 11:15:13 +0800

    es6支持

--- react-demo/src/routes/Home/index.js
+++ react-demo/src/routes/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { HelloWorld } from 'rzx-hello-world';
 import './index.less';
 
 class Home extends Component {
   render () {
     return (
       <div>
+        <HelloWorld />
         <div className='img' />
       </div>
     );
